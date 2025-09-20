a41b816115c824b6f594630866a363a16de4b52f
dahyun_0_1 <changum0520@gmail.com>
2025-09-20 14:04:15 +0900

    재귀 연습문제(BOJ 1629 곱셈)
    스스로 한번 더 구현해볼 것
    모듈러 연산 성질 복습 필요

---
+++ CppStudy/CodingTest/recursion/BOJ_1629.cpp
@@ -0,0 +1,24 @@
+﻿// 0x0B 재귀
+// boj 1629 곱셈
+
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+ll pow(ll a, ll b, ll c) {
+	if (b == 1) return a % c;
+	ll val = pow(a, b / 2, c);
+	val = val * val % c;
+	if (b % 2 == 0) return val;
+	return val * a % c;
+}
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	ll a, b, c;
+	cin >> a >> b >> c;
+
+	cout << pow(a, b, c);
+	return 0;
+}
