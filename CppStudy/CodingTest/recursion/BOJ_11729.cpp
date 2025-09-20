cd0ad4838e94283596afb4780069ed9d7115679a
dahyun_0_1 <changum0520@gmail.com>
2025-09-20 14:05:43 +0900

    재귀 연습문제(BOJ 11729 하노이탑)
    스스로 한번 더 구현해볼 것
    아직 백준에서 통과 못함 이유 알아보기

---
+++ CppStudy/CodingTest/recursion/BOJ_11729.cpp
@@ -0,0 +1,35 @@
+﻿// 0x0B 재귀
+// boj 11729 하노이 탑
+
+#include <iostream>
+using namespace std;
+void func(int a, int b, int n);
+
+int main(void) {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	/*long long moves = (1LL << n) - 1;
+	
+	if (n > 20) {
+		cout << moves << '\n';
+		return 0;
+	}*/
+
+	cout << (1 << n) - 1 << '\n';
+	func(1, 3, n);
+}
+
+void func(int a, int b, int n) {
+	if (n == 1) {
+		cout << a << ' ' << b << '\n';
+		return;
+	}
+
+	func(a, 6 - a - b, n-1);
+	cout << a << ' ' << b << '\n';
+	func(6 - a - b, b, n-1);
+}
